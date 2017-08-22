b0269994fd1633733689311ae308f094cc0819ab
davidmgudeman <davidmgudeman@gmail.com>
2017-08-22 14:26:28 -0500

    exercise 4 - Dependency Injection completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Contact } from  './models/contact'
+import { ContactsService } from './contacts.service'
 
 @Component({
   selector: 'trm-contacts-app',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class ContactsAppComponent {
-  title = 'Angular Master Class setup works!';
-}
+  contacts: Array<Contact>
+
+  constructor(
+     private contactsService: ContactsService
+  ){}
+
+  ngOnInit() {
+    this.contacts = this.contactsService.getContacts()
+  }
+  trackByContacts(index: number | string, contact: Contact): number | string { return  contact.id; }
+
+  
+}  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ContactsService } from './contacts.service'
 
 import { ContactsAppComponent } from './app.component';
+import { Contact } from './models/contact'
 
-@NgModule({
+@NgModule( {
   declarations: [ContactsAppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+  ],
+  providers: [
+    ContactsService, 
   ],
   bootstrap: [ContactsAppComponent]
 })
