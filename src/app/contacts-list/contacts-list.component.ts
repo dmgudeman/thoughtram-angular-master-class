4d55901c3c0322bf8185b502b0ad075bdc315999
davidmgudeman <davidmgudeman@gmail.com>
2017-08-23 15:26:47 -0500

    Using switchmap and merge to get rid of search function

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -5,6 +5,8 @@ import { Observable }        from 'rxjs/Observable';
 import { Subject }        from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/merge';
 
 @Component({
   selector: 'trm-contacts-list',
@@ -12,21 +14,24 @@ import 'rxjs/add/operator/distinctUntilChanged';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-contacts:Observable<Array<Contact>>
-private terms$ = new Subject<string>();
 
+// contacts$: Observable<Array<Contact>>
+private terms$ = new Subject<string>();
+   contacts$: Observable<any>;
   constructor(
      private contactsService: ContactsService
   ){}
 
-  search(e) {
-    this.contacts = this.contactsService.search(e)
-  }
+  // search(e) {
+  //   this.contacts$ = this.contactsService.search(e)
+  // }
   ngOnInit() {
-    this.contacts = this.contactsService.getContacts();
     
-    this.terms$.debounceTime(400)
+    // this.contacts = this.contactsService.getContacts();
+    
+   this.contacts$ = this.terms$.debounceTime(400)
     .distinctUntilChanged()
-    .subscribe(term => this.search(term));
+    .switchMap(item => this.contactsService.search(item))
+    .merge( this.contactsService.getContacts());
   }
 }
