e5bd0586e568507676e9a91898f3a387584a63bb
davidmgudeman <davidmgudeman@gmail.com>
2017-08-24 06:12:16 -0500

    finished the seperation of presentation view and view component exercise 1

--- src/app/contacts-details/contacts-details.component.ts
+++ src/app/contacts-details/contacts-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ContactsService } from '../contacts.service'
 import { Contact } from '../models/contact';
@@ -8,20 +8,7 @@ import { Contact } from '../models/contact';
   templateUrl: './contacts-details.component.html',
   styleUrls: ['./contacts-details.component.css']
 })
-export class ContactsDetailsComponent implements OnInit {
-  contact: Contact
-  constructor(
-    private contactsService: ContactsService,
-    private route: ActivatedRoute,
-  ) { }
-
-  ngOnInit() {
-    let id = this.route.snapshot.paramMap.get('id');
-    this.contactsService.getContact(id).subscribe(
-      (data)=>{
-         this.contact = data
-      }
-    )
-  }
-
+export class ContactsDetailsComponent {
+  @Input() contact: Contact
+ 
 }
